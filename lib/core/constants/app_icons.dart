1123539496f37a49199cee0f3a6d9c35a0a46890
Deepika Yadav <YOUR_GITHUB_EMAIL>
2026-08-10 00:11:22 +0530

    feat: app icons

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const settings = Icons.settings;
+  static const people = Icons.people;
+  static const peopleOutline = Icons.people_outline;
+  static const person = Icons.person;
+  static const personOutline = Icons.person_outline;
+}
