3a733edb1f30994c6720c4d7882c848e80585f82
Deepika Yadav <YOUR_GITHUB_EMAIL>
2026-08-10 00:11:36 +0530

    fix: extension

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -3,10 +3,14 @@ import 'package:provider/provider.dart';
 import '../../providers/language_provider.dart';
 import '../../providers/theme_provider.dart';
 
-extension AppContext on BuildContext {
-  LanguageProvider get language => read<LanguageProvider>();
-  LanguageProvider get languageWatch => watch<LanguageProvider>();
+class AppContext {
+  AppContext(this.context);
 
-  ThemeProvider get theme => read<ThemeProvider>();
-  ThemeProvider get themeWatch => watch<ThemeProvider>();
+  final BuildContext context;
+
+  LanguageProvider get language => context.read<LanguageProvider>();
+  LanguageProvider get languageWatch => context.watch<LanguageProvider>();
+
+  ThemeProvider get theme => context.read<ThemeProvider>();
+  ThemeProvider get themeWatch => context.watch<ThemeProvider>();
 }
